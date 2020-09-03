a6e1125244e90363bf329700e3b90690735498f1
hrjeon <dj.hongten@gmail.com>
2020-09-03 18:28:14 +0900

    GoAlone init

---
+++ GoAlone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoAlone
+//
+//  Created by HongYeol Jeon on 2020/09/03.
+//  Copyright © 2020 HongYeol Jeon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
